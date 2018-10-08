bf945f7e24ec940a4a45ca95454a2275877f79f9
anirudhmuhnot <anirudhmuhnot30u30@gmail.com>
2018-10-08 17:49:21 +0530

    Initial Commit

---
+++ programming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  programming
+//
+//  Created by Anirudh Muhnot on 08/10/18.
+//  Copyright © 2018 Anirudh Muhnot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
